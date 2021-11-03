ea312f4ed64773e8029af0d85504a4b914952e7b
Yuda Fatah <yudafatah@gmail.com>
2021-11-03 08:49:14 +0700

    add block and transaction entities

---
+++ YudaCoin.Api/Entities/Yudacoin/Block.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace YudaCoin.Api.Entities.Yudacoin
+{
+    public class Block
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public int Index { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime Timestamp { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public int Proof { get; set; }
+        public string PreviousHash { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Index} [{Timestamp.ToString("yyyy-MM-dd HH:mm:ss")}] Proof: {Proof} | PrevHash: {PreviousHash}";
+        }
+    }
+}
---
+++ YudaCoin.Api/Entities/Yudacoin/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace YudaCoin.Api.Entities.Yudacoin
+{
+    public class Transaction
+    {
+        [BsonId]
+        public string Id { get; set; }
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Amount { get; set; }
+        public string Recipient { get; set; }
+        public string Sender { get; set; }
+        public string Signature { get; set; }
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Fees { get; set; }
+
+        public override string ToString()
+        {
+            return Amount.ToString("0.00000000") + Recipient + Sender;
+        }
+    }
+}
