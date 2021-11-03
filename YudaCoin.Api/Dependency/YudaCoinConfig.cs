b58ebff7b95dad6f11f32279648606ade24e414b
Yuda Fatah <yudafatah@gmail.com>
2021-11-03 08:48:50 +0700

    add yudacoin IoC

---
+++ YudaCoin.Api/Dependency/YudaCoinConfig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YudaCoin.Api.Configuration;
+using YudaCoin.Api.Repository.Connection.Mongo;
+using YudaCoin.Api.Repository.ConnectionContracts.Mongo;
+
+namespace YudaCoin.Api.Dependency
+{
+    public class YudaCoinConfig
+    {
+        private readonly IServiceCollection _container;
+
+        public YudaCoinConfig(IServiceCollection container)
+        {
+            _container = container;
+        }
+
+        public void RegisterRepositories()
+        {
+            _container.AddSingleton<IYudacoinConnection, YudacoinConnection>();
+        }
+
+        public void RegisterApi()
+        {
+            RegisterRepositories();
+        }
+
+        public void LoadConfiguration(IConfiguration configuration)
+        {
+            var mongoConfig = configuration.GetSection("MongoConfig");
+
+            _container.Configure<MongoConfig>(mongoConfig);
+        }
+    }
+}
